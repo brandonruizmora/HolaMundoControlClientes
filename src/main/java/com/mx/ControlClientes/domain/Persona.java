1d822355b5a5d4c9d10d9ad2dc876a0db99b70c9
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-21 10:41:07 -0500

    Added domain persona and used in controller to display in view

---
+++ src/main/java/com/mx/ControlClientes/domain/Persona.java
@@ -0,0 +1,13 @@
+package com.mx.ControlClientes.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+}
--- src/main/java/com/mx/ControlClientes/web/ControladorInicio.java
+++ src/main/java/com/mx/ControlClientes/web/ControladorInicio.java
@@ -1,13 +1,24 @@
 package com.mx.ControlClientes.web;
 
+import com.mx.ControlClientes.domain.Persona;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControladorInicio {
 
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
+
+        Persona persona = new Persona();
+        persona.setNombre("Brandon");
+        persona.setApellido("Ruiz");
+        persona.setEmail("bruiz@email.com");
+        persona.setTelefono("4494068372");
+
+        model.addAttribute("persona", persona);
+
         return "index";
     }
 
